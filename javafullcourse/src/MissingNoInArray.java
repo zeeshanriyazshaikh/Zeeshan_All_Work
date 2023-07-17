7b891115daddcba27dd7228bad814b31f4b937a3
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2023-07-17 15:43:24 +0530

    Update MissingNoInArray.java
    information added

--- javafullcourse/src/MissingNoInArray.java
+++ javafullcourse/src/MissingNoInArray.java
@@ -6,9 +6,9 @@ public class MissingNoInArray {
 		
 int[] a= {1,2,3,4,6}; //5 is missing
 
-int n=a.length+1;
+int n=a.length+1; //5+1=6
 
-int sum=(n*(n+1))/2;
+int sum=(n*(n+1))/2; //21 [this formulae takes some of above array ex:1+2+3+4+5+6=21]
 
 for(int i=0;i<a.length;i++) {
 	
