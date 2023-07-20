2e51e9e3aeb0788cba7f82830e396917cd98dc2c
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2023-07-20 19:06:58 +0530

    Update MissingNoInArray.java

--- javafullcourse/src/MissingNoInArray.java
+++ javafullcourse/src/MissingNoInArray.java
@@ -20,3 +20,20 @@ System.out.println(sum);
 	}
 
 }
+/* 
+int[] a={1,2,3,4,6};
+        int sum1 = 0;
+        for(int i=0;i<a.length;i++)
+        {
+            sum1=sum1+a[i];
+        }
+        System.out.println(sum1);
+        int sum2 = 0;
+        for(int i=1;i<=6;i++)
+        {
+            sum2=sum2+i;
+        }
+        System.out.println(sum2);
+
+        System.out.println("missing number is "+ (sum2-sum1));
+*/
