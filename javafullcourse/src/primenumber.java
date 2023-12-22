b355e27f589f89218e508419d92cab664d74c0f0
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2023-12-22 14:43:20 +0530

    Update primenumber.java

--- javafullcourse/src/primenumber.java
+++ javafullcourse/src/primenumber.java
@@ -25,6 +25,31 @@ public class primenumber {
 		{
 			System.out.println( n + " no is not prime");
 		}
+
+/*
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        for (int n = 50; n <= 100; n++) {
+            int t = 0;
+
+            for (int i = 2; i <= n - 1; i++) {
+                if (n % i == 0) {
+                    t = t + 1; // come out of loop if this is not satisfied
+                    break;     // optimize by breaking out of the loop as soon as a divisor is found
+                }
+            }
+
+            if (t == 0) {
+                System.out.println(n + " is prime");
+            } else {
+                System.out.println(n + " is not prime");
+            }
+        }
+    }
+}
+
+*/
 		
 		
 		
