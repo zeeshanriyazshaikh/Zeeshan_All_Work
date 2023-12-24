86371d4accc23d416ba84112b1cb4d9899a8de3e
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2023-12-24 15:47:41 +0530

    Update StringClass.java

--- javafullcourse/src/StringClass.java
+++ javafullcourse/src/StringClass.java
@@ -20,6 +20,33 @@ else
 {
 	System.out.print("both are not equal");     	
 }
+		//STRING MANIPULATION
+        String str = "Zeeshan has 4 years of experience";
+        String str1 = "Zeeshan has 4 years of experience";
+        String str2 = "Zeeshan HAS 4 years of experience";
+
+        System.out.println(str.length()); //--O/P "33"
+        System.out.println(str.charAt(3)); //--O/P "s"
+        System.out.println(str.indexOf("s")); //--O/P "3"  [1st occurence of "s"]
+        System.out.println(str.indexOf("s", 9)); //--O/P "10" [2nd occurence of "s"]
+        int secondOccurence = str.indexOf("s", str.indexOf("s") + 1); //--O/P "10"  [2nd occurence of "s" but without hard code value]
+        System.out.println(secondOccurence);
+        System.out.println(str.indexOf("s", secondOccurence + 1)); //--O/P "18"  [3rd occurence of "s"]
+        System.out.println(str.indexOf("has")); //--O/P "8" [Occurence of string]
+        System.out.println(str.indexOf("Hello")); //--O/P "-1" [unavailable string]
+        System.out.println(str.equals(str1));//--O/P "true" [2 string comparison ]
+        System.out.println(str.equalsIgnoreCase(str1));//--O/P "true" [2 string comparison with ignore case]
+        
+        //COMPILER START COMPILING FROM LEFT TO RIGHT
+        String x = "Hello";
+        String y = "World";
+        int a = 100;
+        int b = 200;
+        System.out.println(x+y); //O/P--"HelloWorld"
+        System.out.println(a+b); //O/P--"300"
+        System.out.println(x+y+a+b); //O/P--"HelloWorld100200"
+        System.out.println(a+b+x+y); //O/P--"300HelloWorld"
+        System.out.println(x+y+(a+b)); //O/P--"HelloWorld300"
 	}
 
 }
