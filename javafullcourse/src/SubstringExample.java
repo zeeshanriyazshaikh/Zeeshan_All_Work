ce8d1889a40ccaced6152c949353f376f3d6321b
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-01-31 01:43:16 +0530

    Update SubstringExample.java

--- javafullcourse/src/SubstringExample.java
+++ javafullcourse/src/SubstringExample.java
@@ -2,26 +2,18 @@
 public class SubstringExample {
 
     public static void main(String[] args) {
+//TOGGLE STRING USING SUBSTRING
+        
+        String str = "zEEshan";
+        String str1=str.substring(0,1).toUpperCase(); // output = Z [start index 0 i.e. z and end index 1 i.e. E]
+        System.out.println(str1);
 
-        String originalString = "zEEshan";
+        String str2=str.substring(1,3).toLowerCase();// output = ee [start index 1 i.e. first E and end index 3 i.e. s]
+        System.out.println(str2);
 
-        // Using substring(startIndex)
-
-        String substring1 = originalString.substring(3);
-        String substring1Upper=substring1.toUpperCase();
-        System.out.println(substring1Upper);
-
-        String substring2 = originalString.substring(1, 3);
-        String substring2Lower=substring2.toLowerCase();
-        System.out.println(substring2Lower);
-
-        String substring3 = originalString.substring(0,1);
-        String substring3FirstLetter=substring3.toUpperCase();
-        System.out.println(substring3FirstLetter);
-
-        System.out.println(substring3FirstLetter+substring2Lower+substring1Upper);
-
-
+        String str3=str.substring(3).toUpperCase();// output = SHAN [start index 3 i.e. s and end index till last]
+        System.out.println(str3);
+        System.out.println(str1+str2+str3); // output == ZeeSHAN
 
     }
 
