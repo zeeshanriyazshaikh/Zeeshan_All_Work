78759dafc0f9fabb2d53d7f07ff2055e5b346e5a
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-02-14 19:44:06 +0530

    Create HeadlessBrowser.java

---
+++ src/HeadlessBrowser.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+public class HeadlessBrowserExample {
+   public static void main(String[] args) {
+       // Set path to Chrome driver executable
+       System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+       // Set Chrome options to run headlessly
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--headless"); // This line makes it headless
+       // Initialize Chrome driver
+       WebDriver driver = new ChromeDriver(options);
+       // Open a website
+       driver.get("https://www.example.com");
+       // Print page title
+       System.out.println("Page title is: " + driver.getTitle());
+       // Close browser
+       driver.quit();
+   }
+}
