5734a2c11d81f49d6f8517061ad28d6c36ab5d3e
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-03-04 16:44:12 +0530

    Update StringReverse.java
    One more reverse string code added.

--- javafullcourse/src/StringReverse.java
+++ javafullcourse/src/StringReverse.java
@@ -13,6 +13,30 @@ public class StringReverse {
 			
 			char rev =str.charAt(i);
 			System.out.print(rev);
+/*
+public class MyClass {
+    String rev="";
+    public void isPalindrome(String s){
+        for(int i=0;i<s.length();i++){
+            rev=s.charAt(i)+rev;
+            System.out.println(rev);
+        }
+          System.out.println(rev);
+        if(s.equals(rev)){
+            System.out.print("it is palindrome");
+        }
+        else{
+             System.out.print("it is not palindrome");
+        }
+    }
+    public static void main(String args[]) {
+        String str="zeez";
+        MyClass c=new MyClass();
+        c.isPalindrome(str);
+    }
+}
+*/
+
 
 /* #Reverse-Palindrome-String[convert char to string]
        String str = "MADAM";
