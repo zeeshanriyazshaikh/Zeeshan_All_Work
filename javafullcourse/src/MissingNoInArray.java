30fc6900b127b08cafde23247164f439b418ee23
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-04-04 17:25:32 +0530

    Update MissingNoInArray.java

--- javafullcourse/src/MissingNoInArray.java
+++ javafullcourse/src/MissingNoInArray.java
@@ -3,6 +3,7 @@ public class MissingNoInArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+#FIND MISSING NUMBER IN ARRAY
 		
 int[] a= {1,2,3,4,6}; //5 is missing
 
@@ -21,6 +22,7 @@ System.out.println(sum);
 
 }
 /* 
+# ALTERNATE METHOD TO FIND MISSING NUMBER
 int[] a={1,2,3,4,6};
         int sum1 = 0;
         for(int i=0;i<a.length;i++)
@@ -38,6 +40,7 @@ int[] a={1,2,3,4,6};
         System.out.println("missing number is "+ (sum2-sum1));
 */
 /*
+#FIND MISSING NUMBER SAME AS ABOVE BUT USING FOR-EACH LOOP
 public class MyClass {
     public static void main(String args[]) {
         int[] arr={1,2,3,4,6,7};
@@ -53,3 +56,31 @@ public class MyClass {
 }
 }
 */
+
+# LIST OF ALL MISSING NUMBERS
+
+import java.util.*;
+
+public class listOfMissingNumbers {
+   public static void main(String[] args) {
+       int[] nums = {10, 1, 5, 4, 6}; // Example array
+
+       Set<Integer> set = new HashSet<>();
+       List<Integer> missing = new ArrayList<>();
+
+       int max = Arrays.stream(nums).max().orElse(0); // Find the maximum number in the array
+System.out.println("Max " + max);
+       for (int num : nums) {
+           set.add(num);
+       }
+System.out.println("Set " + set);
+       for (int i = 1; i <= max; i++) {
+           if (!set.contains(i)) {
+               missing.add(i);
+           }
+       }
+
+       System.out.println("Missing numbers: " + missing);
+   }
+} 
+/*
