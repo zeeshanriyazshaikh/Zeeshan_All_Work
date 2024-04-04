f450747e3ddd9612fb10ee7261e66f2ba63e692f
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-04-04 17:45:08 +0530

    Update StringReverse.java

--- javafullcourse/src/StringReverse.java
+++ javafullcourse/src/StringReverse.java
@@ -13,26 +13,27 @@ public class StringReverse {
 			
 			char rev =str.charAt(i);
 			System.out.print(rev);
+
 /*
+#REVERSE STRING USING EMPTY STRING
+
 public class MyClass {
+public static void main(String args[]) {
     String rev="";
-    public void isPalindrome(String s){
-        for(int i=0;i<s.length();i++){
-            rev=s.charAt(i)+rev;
+    String str="zeez";
+    
+        for(int i=0;i<str.length();i++){
+            rev=str.charAt(i)+rev;
             System.out.println(rev);
         }
-          System.out.println(rev);
-        if(s.equals(rev)){
+
+        if(str.equals(rev)){
             System.out.print("it is palindrome");
         }
         else{
              System.out.print("it is not palindrome");
         }
-    }
-    public static void main(String args[]) {
-        String str="zeez";
-        MyClass c=new MyClass();
-        c.isPalindrome(str);
+    
     }
 }
 */
@@ -40,10 +41,11 @@ public class MyClass {
 
 /* #Reverse-Palindrome-String[convert char to string]
        String str = "MADAM";
-        int strLen = str.length();
+       int strLen = str.length();
+
         for (int i = strLen - 1; i >= 0; i--) {
             char str1 = str.charAt(i);
-            String str2 = String.valueOf(str1);
+            String str2 = String.valueOf(str1);   //char to string//we can also use primitive to string conversion 
             System.out.print(str2);
         }
       
