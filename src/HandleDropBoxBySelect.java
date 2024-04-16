2037a22fc9a5be5b1be3a4082b48170839521fce
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-04-16 18:01:49 +0530

    Update and rename HandleDropBox.java to HandleDropBoxBySelect.java

--- src/HandleDropBoxBySelect.java
+++ src/HandleDropBoxBySelect.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class HandleDropBox {
+public class HandleDropBoxBySelect {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Faiz Riyaz\\\\Desktop\\\\chrome driver\\\\chromedriver.exe");
+		
+                System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Faiz Riyaz\\\\Desktop\\\\chrome driver\\\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://signup.ebay.com/pa/crte?ru=https%3A%2F%2Fwww.ebay.com%2F");
 		driver.findElement(By.id("businessaccount-radio")).click();
 		driver.findElement(By.id("businessName")).sendKeys("zeeshan");
 		driver.findElement(By.id("businessEmail")).sendKeys("zee.shaikcool786@gmail.com");
 		
-		//Handle Drop Down
+	        //Handle Drop Down
 		Select select=new Select(driver.findElement(By.id("businessCountry"))); //select is class used for drop down ,and in object we are passing find element by id.
 		
 		select.selectByVisibleText("Austria");
@@ -32,7 +32,7 @@ System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Faiz Riyaz\\\\Desk
 		for(int i=0; i<lists.size();i++)   //we can also write  (int i=1; i<=linklists.size();i++)
 		{
 		String linktext  =	lists.get(i).getText();
-	System.out.println(linktext);
+	        System.out.println(linktext);
 		}
 		
 		
