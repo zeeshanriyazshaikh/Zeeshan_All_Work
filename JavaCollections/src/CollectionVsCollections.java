f995bb835633d98b6fe488a66b074c927e3a8f45
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-04-18 15:31:52 +0530

    Update CollectionVsCollections.java

--- JavaCollections/src/CollectionVsCollections.java
+++ JavaCollections/src/CollectionVsCollections.java
@@ -12,11 +12,15 @@ class HelloWorld {
     }
 }
 #THEORY:
+
 A]Collection
 1>Root interface in the collection hierarchy.
 2>It extends Iterable.
 3>As it is a interface it contains default public and abstract method but after jdk 1.8 it contains static and default method also. 
+4>List,Set,Queue etc.
+	
 B]Collections
 1>It is a utility class in java which contains only static methods.
 2>It extends Objects.
-3>It has only static methods for example: Sorting,Reversing,Shuffling,Searching,Max,Min,Frequency,Copy and Fill,Unmodifiable Collections.
+3>It has only static methods 
+4>For example: Sorting,Reversing,Shuffling,Searching,Max,Min,Frequency,Copy and Fill,Unmodifiable Collections.
