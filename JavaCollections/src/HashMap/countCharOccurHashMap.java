7e2c71d51bf0984e729a71c6bad88d0067252e99
zeeshanriyazshaikh <103321615+zeeshanriyazshaikh@users.noreply.github.com>
2024-07-13 03:03:38 -0700

    Update countCharOccurHashMap.java

--- JavaCollections/src/HashMap/countCharOccurHashMap.java
+++ JavaCollections/src/HashMap/countCharOccurHashMap.java
@@ -9,7 +9,7 @@ public class CharOccurrence {
         // Iterate through each character in the string
         for (char c : str.toCharArray()) {
             // Check if the character is a letter
-            if (Character.isLetter(c)) {
+            if (Character.isLetter(c)) {                                 //for numbers: if (Character.isDigit(c)) 
                 // Convert to lowercase for case-insensitive counting
                 c = Character.toLowerCase(c);
                 // If the character is already present in the map, increment its count
@@ -23,4 +23,25 @@ public class CharOccurrence {
         }
     }
 }
+
+//TO FIND PARTICULAR CHARATER COUNT
+/*
+String str = "Hello w@@or!@ld 1123";
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        char[] a = {'!', '@'};
+        // Iterate through each character in the string
+        for(char b:str.toCharArray()) {
+            for (char d : a) {
+                if (b == d) {
+                    charCountMap.put(d, charCountMap.getOrDefault(d, 0)+1);
+//                    System.out.println(charCountMap);
+                }
+            }
+        }
+        for (Map.Entry entry : charCountMap.entrySet()){
+            System.out.println("count of " + entry.getKey() + " is :- " + entry.getValue());
+        }
+*/
 //NOTE:If we just want to remove space count then use if(c != ' ')
+
+
